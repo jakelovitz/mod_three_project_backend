f717a14f8b1eb1154f124aa091d6cb3fbae193ff
Jake <jakelovitz@gmail.com>
2019-06-17 10:41:10 -0400

    updated model and controller to adhere to naming conventions

---
+++ app/models/api/v1/score.rb
@@ -0,0 +1,3 @@
+class Api::V1::Score < ApplicationRecord
+    belongs_to :user
+end
--- test/controllers/api/v1/scores_controller_test.rb
+++ test/controllers/api/v1/scores_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class Api::V1::ScoresControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
