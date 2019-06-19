da0f3f3af8ee25900af5ecad5c1130302e2ca166
Kirbyasdf <kirbyAugustus@gmail.com>
2019-06-18 23:23:32 -0400

    fixed some of database, need more

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::PeopleController < ApplicationController
 
     def index
-        @person = Person.all
-        render json: @person
+        @people = Person.all
+        render json: @people
     end
 
     def show
     end
 
     private
-   
+
     def find_person
       @person = Person.find(params[:id])
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,32 @@
-sam = Person.create(name: "Sam", occupation: "Skier")
+sam = Person.create!(name: "Sam", occupation: "Skier")
 
-sarah = Person.create(name: "Sarah", occupation: "Officer of the Law")
-
-
-
-Wound.create(name: "right arm", description: "right arm description", location: "right arm", img_url: "https://i.imgur.com/2FlwKX3.png", person_id: sam.id)
-
-Wound.create(name: "left arm", description: "left arm description", location: "left arm", img_url: "https://i.imgur.com/f5prGHs.png", person_id: sam.id)
-
-Wound.create(name: "right leg", description: "right leg description", location: "right leg", img_url: "https://i.imgur.com/VRUFDLE.png", person_id: sam.id)
-
-Wound.create(name: "left leg", description: "left leg description", location: "left leg", img_url: "https://i.imgur.com/GDOCNyl.png", person_id: sam.id)
-
-Wound.create(name: "body", description: "body description", location: "body", img_url: "https://i.imgur.com/5RQ9MEO.png", person_id: sam.id)
-
-Wound.create(name: "head", description: "head description", location: "head", img_url: "https://i.imgur.com/SA2g6vW.png", person_id: sam.id)
+sarah = Person.create!(name: "Sarah", occupation: "Officer of the Law")
 
 
 
+Wound.create!(name: "right arm", description: "right arm description", location: "right arm", img_url: "https://i.imgur.com/2FlwKX3.png", person_id: sam.id)
 
-Wound.create(name: "right arm", description: "right arm description", location: "right arm", img_url: "https://i.imgur.com/2FlwKX3.png", person_id: sarah.id)
+Wound.create!(name: "left arm", description: "left arm description", location: "left arm", img_url: "https://i.imgur.com/f5prGHs.png", person_id: sam.id)
 
-Wound.create(name: "left arm", description: "left arm description", location: "left arm", img_url: "https://i.imgur.com/f5prGHs.png", person_id: sarah.id)
+Wound.create!(name: "right leg", description: "right leg description", location: "right leg", img_url: "https://i.imgur.com/VRUFDLE.png", person_id: sam.id)
 
-Wound.create(name: "right leg", description: "right leg description", location: "right leg", img_url: "https://i.imgur.com/VRUFDLE.png", person_id: sarah.id)
+Wound.create!(name: "left leg", description: "left leg description", location: "left leg", img_url: "https://i.imgur.com/GDOCNyl.png", person_id: sam.id)
 
-Wound.create(name: "left leg", description: "left leg description", location: "left leg", img_url: "https://i.imgur.com/GDOCNyl.png", person_id: sarah.id)
+Wound.create!(name: "body", description: "body description", location: "body", img_url: "https://i.imgur.com/5RQ9MEO.png", person_id: sam.id)
 
-Wound.create(name: "body", description: "body description", location: "body", img_url: "https://i.imgur.com/5RQ9MEO.png", person_id: sarah.id)
+Wound.create!(name: "head", description: "head description", location: "head", img_url: "https://i.imgur.com/SA2g6vW.png", person_id: sam.id)
 
-Wound.create(name: "head", description: "head description", location: "head", img_url: "https://i.imgur.com/SA2g6vW.png", person_id: sarah.id)
+
+
+
+Wound.create!(name: "right arm", description: "right arm description", location: "right arm", img_url: "https://i.imgur.com/2FlwKX3.png", person_id: sarah.id)
+
+Wound.create!(name: "left arm", description: "left arm description", location: "left arm", img_url: "https://i.imgur.com/f5prGHs.png", person_id: sarah.id)
+
+Wound.create!(name: "right leg", description: "right leg description", location: "right leg", img_url: "https://i.imgur.com/VRUFDLE.png", person_id: sarah.id)
+
+Wound.create!(name: "left leg", description: "left leg description", location: "left leg", img_url: "https://i.imgur.com/GDOCNyl.png", person_id: sarah.id)
+
+Wound.create!(name: "body", description: "body description", location: "body", img_url: "https://i.imgur.com/5RQ9MEO.png", person_id: sarah.id)
+
+Wound.create!(name: "head", description: "head description", location: "head", img_url: "https://i.imgur.com/SA2g6vW.png", person_id: sarah.id)
